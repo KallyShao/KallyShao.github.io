e7fc7d040122c6980a6e98e5936cff27fe9677d1
KallyShao <188963758@qq.com>
2017-09-19 23:44:49 +0800

    提交bee, groupbuy

---
+++ web_development/groupbuy/js/index.js
@@ -0,0 +1,7 @@
+window.onload = function(){
+	var oBanner = document.getElementById("banner-img");
+	var aImg = oBanner.getElementsByTagName("img");
+	var timer = setInterval(function(){
+		var i = 0;
+	}, 1000)
+}
